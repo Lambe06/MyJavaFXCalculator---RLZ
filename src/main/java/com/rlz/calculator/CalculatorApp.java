3cad46736472dcdd25c9d32a33f4b1a2b045758e
Lambe06 <rzlatan2016@gmail.com>
2026-05-03 14:45:55 +0200

    Refactor CalculatorApp.java formatting and structure

--- src/main/java/com/rlz/calculator/CalculatorApp.java
+++ src/main/java/com/rlz/calculator/CalculatorApp.java
@@ -10,14 +10,14 @@ import java.io.IOException;
 public class CalculatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // Carica il file FXML che sta in resources/com/rlz/calculator/
+        
         FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApp.class.getResource("/com/rlz/calculator/interface.fxml"));
-        // Sostituisci 350 e 500 con le dimensioni reali della tua calcolatrice
+        
         Scene scene = new Scene(fxmlLoader.load(), 350, 500);
 
         stage.setTitle("MyCalculator 1.0 - RLZ");
         stage.setScene(scene);
-        stage.setResizable(false); // Layout non modificabile
+        stage.setResizable(false); 
         stage.show();
     }
 
